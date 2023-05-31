e937ddc52575af472404a81da0bf2363f8e64749
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2023-05-31 13:22:59 +0200

    Update jazzer-api docs for v0.17.0

--- jazzer-api/package-search-index.js
+++ jazzer-api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.code_intelligence.jazzer.api"},{"l":"jaz"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.code_intelligence.jazzer.api"},{"l":"jaz"}];updateSearchResults();
